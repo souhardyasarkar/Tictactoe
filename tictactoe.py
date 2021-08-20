fb7d7441f86d420578635610a9db8b3a31a2b870
souhardyasarkar <68432043+souhardyasarkar@users.noreply.github.com>
2021-08-20 19:51:43 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,6 @@
 #Implementation of Two Player Tic-Tac-Toe game in Python.
 
-''' We will make the board using dictionary 
-    in which keys will be the location(i.e : top-left,mid-right,etc.)
-    and initialliy it's values will be empty space and then after every move 
-    we will change the value according to player's choice of move. '''
+
 
 theBoard = {'7': ' ' , '8': ' ' , '9': ' ' ,
             '4': ' ' , '5': ' ' , '6': ' ' ,
@@ -14,9 +11,7 @@ board_keys = []
 for key in theBoard:
     board_keys.append(key)
 
-''' We will have to print the updated board after every move in the game and 
-    thus we will make a function in which we'll define the printBoard function
-    so that we can easily print the board everytime by calling this function. '''
+
 
 def printBoard(board):
     print(board['7'] + '|' + board['8'] + '|' + board['9'])
